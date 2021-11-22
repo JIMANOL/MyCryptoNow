be689631990cd722f22a623653939ad9af82248d
JIMANOL <jimanol_cg@hotmail.com>
2021-11-22 11:33:35 -0600

    Interfaz InfoCrypto
    Se agregó el fragment InfoCrypto

---
+++ app/src/main/java/com/example/mycryptonow/ui/infocrypto/InfoCryptoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mycryptonow.ui.infocrypto;
+
+import androidx.lifecycle.ViewModel;
+
+public class InfoCryptoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
