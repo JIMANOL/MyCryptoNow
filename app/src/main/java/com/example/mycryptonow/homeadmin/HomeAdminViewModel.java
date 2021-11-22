ecb72f31eb61e47370b3473c7c1247ac75081135
JIMANOL <jimanol_cg@hotmail.com>
2021-11-22 17:20:33 -0600

    Interfaz Home Admin
    Se agregó la interfaz Home Admin

---
+++ app/src/main/java/com/example/mycryptonow/homeadmin/HomeAdminViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mycryptonow.homeadmin;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeAdminViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/mycryptonow/homeadmin/homeAdmin.java
@@ -0,0 +1,38 @@
+package com.example.mycryptonow.homeadmin;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mycryptonow.R;
+
+public class homeAdmin extends Fragment {
+
+    private HomeAdminViewModel mViewModel;
+
+    public static homeAdmin newInstance() {
+        return new homeAdmin();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_admin_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HomeAdminViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
