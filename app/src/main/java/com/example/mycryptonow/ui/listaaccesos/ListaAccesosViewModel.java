c656c3f94e3b71e110103e8682d0d943ae297592
JIMANOL <jimanol_cg@hotmail.com>
2021-11-27 13:54:19 -0600

    Lista de Accesos
    Se agregó el código para mostrar la lista de accesos

--- app/src/main/java/com/example/mycryptonow/ui/listaaccesos/ListaAccesosViewModel.java
+++ app/src/main/java/com/example/mycryptonow/ui/listaaccesos/ListaAccesosViewModel.java
@@ -1,7 +1,20 @@
 package com.example.mycryptonow.ui.listaaccesos;
 
+
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ListaAccesosViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> mText;
+
+    public ListaAccesosViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is listar fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
--- app/src/main/java/com/example/mycryptonow/ui/listaaccesos/listaAccesosFragment.java
+++ app/src/main/java/com/example/mycryptonow/ui/listaaccesos/listaAccesosFragment.java
@@ -1,7 +1,10 @@
 package com.example.mycryptonow.ui.listaaccesos;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +14,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.mycryptonow.R;
+import com.example.mycryptonow.ui.listaaccesos.Acceso_MB;
+import com.example.mycryptonow.ui.listar.ListarViewModel;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class listaAccesosFragment extends Fragment {
+import java.util.ArrayList;
+
+
+
+public class listaAccesosFragment extends Fragment implements View.OnClickListener{
 
     private ListaAccesosViewModel mViewModel;
 
@@ -22,10 +42,94 @@ public class listaAccesosFragment extends Fragment {
         return new listaAccesosFragment();
     }
 
+   // private ListarViewModel listarViewModel;
+    private ListaAccesosViewModel listarViewModel;
+
+    static ArrayList<Acceso_MB> Accesos = new ArrayList<Acceso_MB>();
+    static ArrayList<String> ListaAccesos = new ArrayList<String>();
+    //public firebase FireBase = new firebase();
+
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    ListView list;
+
+    public void iniciarFirebase(Context contexto) {
+        FirebaseApp.initializeApp(contexto);
+        this.firebaseDatabase = FirebaseDatabase.getInstance();
+        this.databaseReference = firebaseDatabase.getReference();
+        //Toast.makeText(contexto,"test: "+databaseReference.toString(),Toast.LENGTH_LONG).show();
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.lista_accesos_fragment, container, false);
+        ///return inflater.inflate(R.layout.lista_accesos_fragment, container, false);
+        listarViewModel =
+                new ViewModelProvider(this).get(ListaAccesosViewModel.class);
+        View root = inflater.inflate(R.layout.lista_accesos_fragment, container, false);
+        //final TextView textView = root.findViewById(R.id.text_slideshow);
+        listarViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                //textView.setText(s);
+            }
+        });
+        iniciarFirebase(this.getContext());
+        list=root.findViewById(R.id.lvListarAccesos);
+        System.out.println("-->"+databaseReference.toString());
+
+        ////////////////////////////////LISTAR ACCESOS////////////////////////////////////////////
+        databaseReference.child("accesos").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+                ListaAccesos.clear();
+                Accesos.clear();
+                for(DataSnapshot objSnapshot: snapshot.getChildren()){
+                    Acceso_MB acceso = objSnapshot.getValue(Acceso_MB.class);
+                    ListaAccesos.add("(" + acceso.getID() + ") ");
+                    Accesos.add(acceso);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, ListaAccesos);
+                    list.setAdapter(adapter);
+                }
+
+            }
+
+            public void onCancelled(@NonNull DatabaseError error){}
+
+        });
+        ////////////////////////////////////////////////////////////////////////////
+
+        /*if(registros.isEmpty()){
+            sinRegistros.add("Sin Registros");
+            ArrayAdapter<String> adapter = new  ArrayAdapter<String> (getContext(), android.R.layout.simple_list_item_1, sinRegistros);
+            list.setAdapter(adapter);
+        }else {
+            ArrayAdapter<alumno_MB> adapter = new ArrayAdapter<alumno_MB>(getContext(), android.R.layout.simple_list_item_1, registros);
+            list.setAdapter(adapter);
+        }*/
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                View dialogView=LayoutInflater.from(getContext()).inflate(R.layout.cuadro_dialogo,null);
+                String datos =
+                        "ID: "+Accesos.get(i).getID()+"\r\n";
+                datos+="FECHA: "+Accesos.get(i).getFecha()+"\r\n";
+                datos+="HORA: "+Accesos.get(i).getHora()+"\r\n";
+                datos+="DISPOSITIVO: "+Accesos.get(i).getDispositivo()+"\r\n";
+                datos+="UBICACIÓN: "+Accesos.get(i).getUbicacion()+"\r\n";
+                ((TextView)dialogView.findViewById(R.id.tvpInfoDetallada)).setText(datos);
+                AlertDialog.Builder dialogo=new  AlertDialog.Builder(getContext());
+                dialogo.setTitle("Datos del Acceso");
+                dialogo.setView(dialogView);
+                dialogo.setPositiveButton("Aceptar",null);
+                dialogo.show();
+
+            }
+        });
+
+
+        return root;
     }
 
     @Override
@@ -35,4 +139,8 @@ public class listaAccesosFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onClick(View view) {
+
+    }
 }
