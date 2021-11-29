f3a28144a4c4bd78b53123a52239193eb6719203
Jose Enrique Maldonado Verazas <enrique241191@gmail.com>
2021-11-29 00:20:02 -0600

    agregar firebase

--- app/src/main/java/com/example/mycryptonow/ui/agregar/AgregarViewModel.java
+++ app/src/main/java/com/example/mycryptonow/ui/agregar/AgregarViewModel.java
@@ -1,7 +1,36 @@
 package com.example.mycryptonow.ui.agregar;
 
+import android.app.Activity;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.mycryptonow.db.Realtime;
+import com.example.mycryptonow.interfaces.Respuesta;
+import com.example.mycryptonow.models.Datum;
+import com.example.mycryptonow.models.MiCrypto;
+import com.example.mycryptonow.models.Usuario;
+
 public class AgregarViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private MutableLiveData<MiCrypto> miCryptoMutableLiveData = new MutableLiveData<MiCrypto>();
+    private Realtime database= new Realtime();
+
+    public MutableLiveData getCrypto(){
+        return miCryptoMutableLiveData;
+    }
+
+    public void agregarCrypto(MiCrypto miCrypto, Activity activity){
+        database.agregarMiCrypto(miCrypto, activity, new Respuesta() {
+            @Override
+            public void respuesta(Object respuesta) {
+
+            }
+        });
+    }
+
+
+
+
+
 }
