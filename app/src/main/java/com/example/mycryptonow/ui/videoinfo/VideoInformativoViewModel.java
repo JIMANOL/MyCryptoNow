baa668e812094ba744098a11652b18dea4c22fe4
JIMANOL <jimanol_cg@hotmail.com>
2021-11-29 21:41:42 -0600

    Fragment Video
    Se creó el fragment para el video, se agregó al menú y se le dio funcionalidad.

---
+++ app/src/main/java/com/example/mycryptonow/ui/videoinfo/VideoInformativoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mycryptonow.ui.videoinfo;
+
+import androidx.lifecycle.ViewModel;
+
+public class VideoInformativoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
