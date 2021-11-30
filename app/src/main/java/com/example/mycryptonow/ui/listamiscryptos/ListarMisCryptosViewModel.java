7cc9ca5a79d0a51120223537a5e69b679bfa97bb
Jose Enrique Maldonado Verazas <enrique241191@gmail.com>
2021-11-29 18:46:20 -0600

    ListarMisCryptos

--- app/src/main/java/com/example/mycryptonow/ui/listamiscryptos/ListarMisCryptosViewModel.java
+++ app/src/main/java/com/example/mycryptonow/ui/listamiscryptos/ListarMisCryptosViewModel.java
@@ -1,7 +1,15 @@
 package com.example.mycryptonow.ui.listamiscryptos;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.mycryptonow.db.Realtime;
+import com.example.mycryptonow.models.Datum;
+
+import java.util.ArrayList;
+
 public class ListarMisCryptosViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private Realtime database = new Realtime();
+
+    public void buscarMisCryptos(){}
 }
